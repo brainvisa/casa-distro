cd18970c5db056aa7f3248378a83603b4a9f00e1
sapetnioc <yann@cointepas.net>
2017-06-27 14:35:47 +0000

    Implemented pull_build_workflows and upload_build_workflows
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/casa/casa-distro/branches/bug_fix@78162 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/casa_distro/__init__.py
+++ python/casa_distro/__init__.py
@@ -2,9 +2,16 @@ from __future__ import absolute_import
 
 import os
 import os.path as osp
+from glob import glob
 
 from .info import NAME as project_name, version_major, version_minor
 # Find location of the shared directory
 share_directory = osp.join(os.environ.get('BRAINVISA_HOME', osp.dirname(osp.dirname(__file__))), 'share', '%s-%s.%s' % (project_name, version_minor, version_minor))
 linux_os_ids = ['ubuntu-12.04', 'ubuntu-16.04']
 casa_branches = ['latest_release', 'bug_fix']
+
+
+def iter_build_workflow(build_workflows_repository, distro='*', branch='*',
+                        system='*'):
+    for i in glob(osp.join(build_workflows_repository, distro, '%s-%s' % (branch, system), 'conf')):
+        yield osp.dirname(i)
