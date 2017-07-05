aab52f843189afe121dba5ca4277825aca8a04d0
denisri <denis.riviere.gm@gmail.com>
2017-07-05 17:48:56 +0000

    begin build script for light libnetcdf
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/casa/casa-distro/branches/bug_fix@78384 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ share/docker/casa-dev/ubuntu-16.04/build_netcdf.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+# This script builds a minimal libnetcdf, with fewer dependencies than the
+# system one on Ubuntu in /tmp/netcdf_build, then copies libs
+# in /tmp/netcdf.
+# It is intended to run in a docker container (casa-dev image) with /tmp
+# mounted on a host filesystem so that the built libs can be copied somewhere
+# else, typically in the sources of casa-test images.
+# Use the host-side script: build_mesa_host.sh to run this one inside docker.
+
+NETCDF_VERSION=4.4.1.1
+# NETCDF_VERSION=4.1.1
+OLD=old/
+
+cd /tmp
+wget ftp://ftp.unidata.ucar.edu/pub/netcdf/${OLD}netcdf-$NETCDF_VERSION.tar.gz
+tar xvf netcdf-$NETCDF_VERSION.tar.gz
+mkdir netcdf_build
+cd netcdf_build
+cmake ../netcdf-$NETCDF_VERSION -DCMAKE_INSTALL_PREFIX=/tmp/netcdf -DBUILD_TESTING=OFF -DBUILD_TESTSETS=OFF -DBUILD_UTILITIES=OFF -DCMAKE_BUILD_TYPE=Release -DENABLE_DAP=OFF -DENABLE_EXAMPLES=OFF -DENABLE_TESTS=OFF
+make -j4
+make -j4 install
+cd ..
+rm -Rf netcdf_build netcdf-$NETCDF_VERSION
