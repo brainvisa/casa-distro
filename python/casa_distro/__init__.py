458a5f732fe262cff9b5d07d14fa61b306d8b8bf
denisri <denis.riviere.gm@gmail.com>
2018-06-28 10:04:26 +0000

    oops
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/casa-distro/branches/bug_fix@82159 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/casa_distro/__init__.py
+++ python/casa_distro/__init__.py
@@ -7,7 +7,7 @@ from glob import glob
 from .info import NAME as project_name, version_major, version_minor
 # Find location of the shared directory
 share_directory = osp.join(os.environ.get('BRAINVISA_HOME', osp.dirname(osp.dirname(__file__))), 'share', '%s-%s.%s' % (project_name, version_major, version_minor))
-linux_os_ids = ['ubuntu-12.04', 'ubuntu-12.04', 'ubuntu-16.04', 'ubuntu-18.04']
+linux_os_ids = ['ubuntu-12.04', 'ubuntu-14.04', 'ubuntu-16.04', 'ubuntu-18.04']
 casa_branches = ['latest_release', 'bug_fix', 'trunk']
 
 
