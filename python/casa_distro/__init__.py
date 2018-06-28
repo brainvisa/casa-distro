55fb5cd0bf00a566c3714737f2b5665d03568f72
denisri <denis.riviere.gm@gmail.com>
2018-06-28 10:03:05 +0000

    fixes
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/casa-distro/branches/bug_fix@82158 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- python/casa_distro/__init__.py
+++ python/casa_distro/__init__.py
@@ -6,9 +6,9 @@ from glob import glob
 
 from .info import NAME as project_name, version_major, version_minor
 # Find location of the shared directory
-share_directory = osp.join(os.environ.get('BRAINVISA_HOME', osp.dirname(osp.dirname(__file__))), 'share', '%s-%s.%s' % (project_name, version_minor, version_minor))
-linux_os_ids = ['ubuntu-12.04', 'ubuntu-16.04']
-casa_branches = ['latest_release', 'bug_fix']
+share_directory = osp.join(os.environ.get('BRAINVISA_HOME', osp.dirname(osp.dirname(__file__))), 'share', '%s-%s.%s' % (project_name, version_major, version_minor))
+linux_os_ids = ['ubuntu-12.04', 'ubuntu-12.04', 'ubuntu-16.04', 'ubuntu-18.04']
+casa_branches = ['latest_release', 'bug_fix', 'trunk']
 
 
 def iter_build_workflow(build_workflows_repository, distro='*', branch='*',
