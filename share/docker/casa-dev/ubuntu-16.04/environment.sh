c71b5c6c55064a653843c41da3e7b6a498f828b7
sapetnioc <yann@cointepas.net>
2018-11-26 09:43:55 +0000

    Do not set variable if already set to allow runtime customization of these variables with Singularity
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/development/casa-distro/branches/bug_fix@83885 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- share/docker/casa-dev/ubuntu-16.04/environment.sh
+++ share/docker/casa-dev/ubuntu-16.04/environment.sh
@@ -1,21 +1,39 @@
 # Directory containing all files used to configure a build directory (svn passwords, bv_maker.cfg, etc.)
-export CASA_CONF=/casa/conf
+if [ -z "$CASA_CONF" ]; then
+    export CASA_CONF=/casa/conf
+fi
 # Directory containing source code
-export CASA_SRC=/casa/src
+if [ -z "$CASA_SRC" ]; then
+    export CASA_SRC=/casa/src
+fi
 # Directory containing all files that are necessary only for building (source tree, build dir, etc.)
-export CASA_BUILD=/casa/build
+if [ -z "$CASA_BUILD" ]; then
+    export CASA_BUILD=/casa/build
+fi
 # Installation directory
-export CASA_INSTALL=/casa/install
+if [ -z "$CASA_INSTALL" ]; then
+    export CASA_INSTALL=/casa/install
+fi
 # Installation directory
-export CASA_PACK=/casa/pack
+if [ -z "$CASA_PACK" ]; then
+    export CASA_PACK=/casa/pack
+fi
 # Tests data directory
-export CASA_TESTS=/casa/tests
+if [ -z "$CASA_TESTS" ]; then
+    export CASA_TESTS=/casa/tests
+fi
 # Custom projects
-export CASA_CUSTOM_SRC=/casa/custom/src
-export CASA_CUSTOM_BUILD=/casa/custom/build
+if [ -z "$CASA_CUSTOM_SRC" ]; then
+    export CASA_CUSTOM_SRC=/casa/custom/src
+fi
+if [ -z "$CASA_CUSTOM_BUILD" ]; then
+    export CASA_CUSTOM_BUILD=/casa/custom/build
+fi
 
 # Set variable to make bv_maker use /casa/conf/bv_maker.cfg by default
-export BRAINVISA_BVMAKER_CFG="$CASA_CONF/bv_maker.cfg"
+if [ -z "$BRAINVISA_BVMAKER_CFG" ]; then
+    export BRAINVISA_BVMAKER_CFG="$CASA_CONF/bv_maker.cfg"
+fi
 
 export PATH=${PATH}:/casa/brainvisa-cmake/bin
 if [ -f "${CASA_BUILD}/bin/bv_env.sh" ]; then
