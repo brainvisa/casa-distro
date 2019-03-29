0facb32c45e4116d62ef685e28c8bead7f06ca60
Denis Rivière <denis.riviere.gm@gmail.com>
2019-03-29 09:53:55 +0100

    fix for python3

--- python/casa_distro/log.py
+++ python/casa_distro/log.py
@@ -1,24 +1,27 @@
 # coding: utf-8 
 
 def getLogFile(verbose, openmode='w+'):
-    import ast, types, sys
-    
-    if isinstance(verbose, types.StringTypes):
+    import ast, types, sys, six, io
+
+    if isinstance(verbose, six.string_types):
         try:
             # Try to interpret string as boolean or integer values
             verbose = ast.literal_eval(verbose.title())
-            
+
         except:
             # Try to open file from given string
             try:
                 verbose = open(verbose, openmode)
             except:
                 pass
-    
-    if isinstance(verbose, (types.IntType, types.BooleanType)):
+
+    if isinstance(verbose, (int, bool)):
         return sys.stdout if verbose else None
-    
-    if isinstance(verbose, (types.FileType, types.NoneType)):
+
+    if verbose is None \
+            or ((sys.version_info[0] >= 3 and isinstance(verbose, io.IOBase))
+                or (sys.version_info[0] < 3
+                    and isinstance(verbose, types.FileType))):
         return verbose
-    
+
     return None
