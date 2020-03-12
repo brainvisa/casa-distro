4c04ed6d39f2718e134aeec2412e4db32bfa8ef6
Yann Leprince <yann.leprince@cea.fr>
2020-03-12 14:52:04 +0100

    Sort and tidy up APT dependencies in casa-dev:ubuntu-18.04

--- share/docker/casa-dev/ubuntu-18.04/install_apt_dev_dependencies.sh
+++ share/docker/casa-dev/ubuntu-18.04/install_apt_dev_dependencies.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 # Install system dependencies for image cati/casa-dev:ubuntu-18.04
 #
 # NOTE: This script is used to create the casa-dev Docker/Singularity image,
@@ -8,20 +8,18 @@
 set -e  # stop the script on error
 set -x  # display commands before running them
 
-APT_GET="sudo apt-get -o Acquire::Retries=3"
-
 ###############################################################################
 # Install system packages with apt-get
 ###############################################################################
 
 export DEBIAN_FRONTEND=noninteractive
-APT_GET_INSTALL="$APT_GET install --no-install-recommends -y"
+APT_GET_INSTALL="sudo apt-get -o Acquire::Retries=3 install --no-install-recommends -y"
 
 # setup best mirror in /etc/apt/sources.list
 # sudo python2 -m pip --no-cache-dir install apt-mirror-updater
 # sudo apt-mirror-updater -a
 
-$APT_GET update
+sudo apt-get -o Acquire::Retries=3 update
 
 # WARNING: it is necessary to call apt-get install separately for small groups
 # of packages to avoid the mysterious 101st package issue (Download of the
@@ -32,6 +30,9 @@ $APT_GET update
 # case, we have to re-introduce indirect dependencies here, but this time we
 # will *explicitly* mark them as such, to avoid ever having to disentangle this
 # mess again...
+#
+# TODO: check that the APT option -o Acquire::Retries fixes the 101st package
+# issue
 
 # Probably obsolete packages (TODO: remove)
 # $APT_GET_INSTALL apt-utils
@@ -39,50 +40,6 @@ $APT_GET update
 # $APT_GET_INSTALL libgsl-dev  # was used in highres-cortex (only?)
 # $APT_GET_INSTALL openjdk-8-jdk  # was used for Docbook docs
 # $APT_GET_INSTALL pyro  # obsolete since soma-workflow 3
-
-# Dubious packages (TODO: check why they were added / if they are really needed)
-# $APT_GET_INSTALL gdal-data
-# $APT_GET_INSTALL i965-va-driver
-# $APT_GET_INSTALL ibverbs-providers
-# $APT_GET_INSTALL libbdplus0
-# $APT_GET_INSTALL libbluray2
-# $APT_GET_INSTALL libavcodec57
-# $APT_GET_INSTALL libavformat57
-# $APT_GET_INSTALL libchromaprint1
-# $APT_GET_INSTALL libcrystalhd3
-# $APT_GET_INSTALL libgme0
-# $APT_GET_INSTALL libgsm1
-# $APT_GET_INSTALL libmp3lame0
-# $APT_GET_INSTALL libmpg123-0
-# $APT_GET_INSTALL libnl-route-3-200
-# $APT_GET_INSTALL libopenmpt0
-# $APT_GET_INSTALL software-properties-common
-
-# Runtime dependencies (TODO: remove once this image is based on casa-run)
-$APT_GET_INSTALL lftp
-$APT_GET_INSTALL libopenblas-base
-$APT_GET_INSTALL libatlas3-base
-$APT_GET_INSTALL libqwt-qt5-6
-$APT_GET_INSTALL mesa-utils
-$APT_GET_INSTALL net-tools  # really useful?
-$APT_GET_INSTALL python-setuptools  # needed to install source packages from pip
-$APT_GET_INSTALL sqlite3
-$APT_GET_INSTALL unzip
-$APT_GET_INSTALL libaacs0
-$APT_GET_INSTALL libarmadillo8
-$APT_GET_INSTALL libarpack2
-$APT_GET_INSTALL libdap25
-$APT_GET_INSTALL libdapclient6v5
-$APT_GET_INSTALL libdapserver7v5
-$APT_GET_INSTALL libepsilon1
-$APT_GET_INSTALL libflann1.9
-$APT_GET_INSTALL libfreexl1
-$APT_GET_INSTALL mpi-default-bin
-
-# Seemingly redundant packages (TODO: check and remove)
-$APT_GET_INSTALL libgtk2.0  # does not exist??
-$APT_GET_INSTALL libgdk-pixbuf2.0-0
-$APT_GET_INSTALL libgdk-pixbuf2.0-dev
 
 # Source version control
 $APT_GET_INSTALL kdesdk-scripts  # for the colorsvn script
@@ -97,6 +54,7 @@ $APT_GET_INSTALL clang
 $APT_GET_INSTALL cmake
 $APT_GET_INSTALL cmake-curses-gui
 $APT_GET_INSTALL gfortran
+$APT_GET_INSTALL pkg-config
 # Compiled and installed in install_compiled_dev_dependencies.sh because of a
 # bug in the SIP version supplied in APT.
 #
@@ -127,17 +85,15 @@ $APT_GET_INSTALL xterm
 $APT_GET_INSTALL doxygen
 $APT_GET_INSTALL ghostscript
 $APT_GET_INSTALL graphviz
-$APT_GET_INSTALL graphviz-dev
 $APT_GET_INSTALL pandoc
 $APT_GET_INSTALL python-epydoc
 $APT_GET_INSTALL python-sphinx
+$APT_GET_INSTALL texlive-latex-base
 $APT_GET_INSTALL texlive-fonts-recommended
 $APT_GET_INSTALL wkhtmltopdf
 
 # Framework-specific tools
 $APT_GET_INSTALL qt5-default
-$APT_GET_INSTALL qttools5-dev-tools
-$APT_GET_INSTALL qttools5-private-dev
 
 
 # Python 3 packages
@@ -199,138 +155,73 @@ $APT_GET_INSTALL python3-fastcluster
 
 
 
-# Development packages of compiled libraries (C/C++/Fortran) that are used by components of BrainVISA (these components are packaged 
-$APT_GET_INSTALL libdcmtk-dev
-$APT_GET_INSTALL zlib1g-dev
+# Development packages of compiled libraries (C/C++/Fortran) that are used by
+# components of the 'standard' BrainVISA distribution (these are the libraries
+# that are looked up by the find_* statements in the CMake files of BrainVISA
+# projects, excluding the packages that belong to other categories, like Python
+# or documentation building packages).
+brainvisa_standard_dev_dependencies=(
+    libasound2-dev
+    libboost-dev
+    libboost-filesystem-dev
+    libboost-system-dev
+    libdcmtk-dev
+    libexpat1-dev
+    libgl1-mesa-dev
+    libglib2.0-dev
+    libglu1-mesa-dev
+    libjpeg-dev
+    libminc-dev
+    # libnetcdf-dev  # compiled in the casa-run image
+    libomp-dev  # OpenMP with the clang compiler
+    libopenjp2-7-dev
+    libpng-dev
+    libqt5x11extras5-dev
+    libqwt-qt5-dev
+    libsigc++-2.0-dev
+    libsvm-dev
+    libtiff-dev
+    libvtk6-dev
+    libvtk6-qt-dev
+    libxml2-dev
+    qttools5-dev
+    qtmultimedia5-dev
+    qttools5-dev-tools
+    zlib1g-dev
+    # Other packages that are searched in CMake files using find_*, but were
+    # not included in the older casa-dev images:
+    #
+    # libxml++2.6-dev
+    # swig
+)
 
-# 
 
-# TODO: clean up this list of its indirect dependencies
-$APT_GET_INSTALL libsigc++-2.0-dev
-$APT_GET_INSTALL libsqlite3-dev
-$APT_GET_INSTALL libnetcdf-dev
-$APT_GET_INSTALL libreadline-dev
-$APT_GET_INSTALL libboost-dev
-$APT_GET_INSTALL libtiff-dev
-$APT_GET_INSTALL libjpeg-dev
-$APT_GET_INSTALL libpng-dev
-$APT_GET_INSTALL libmng-dev
-$APT_GET_INSTALL libminc-dev
-$APT_GET_INSTALL libxml2-dev
-$APT_GET_INSTALL libsvm-dev
-$APT_GET_INSTALL libltdl7-dev
-$APT_GET_INSTALL libncurses5-dev
-$APT_GET_INSTALL libgtk2.0-dev
-$APT_GET_INSTALL libjpeg-turbo8-dev
-$APT_GET_INSTALL libblas-dev
-$APT_GET_INSTALL liblapack-dev
-$APT_GET_INSTALL libffi-dev
-$APT_GET_INSTALL libmpich-dev
-$APT_GET_INSTALL libgstreamer1.0-dev
-$APT_GET_INSTALL libgstreamer-plugins-base1.0-dev
-$APT_GET_INSTALL liborc-0.4-dev
-$APT_GET_INSTALL libxslt1-dev
-$APT_GET_INSTALL libicu-dev
-$APT_GET_INSTALL x11proto-gl-dev
-$APT_GET_INSTALL libbz2-dev
-$APT_GET_INSTALL libzmq3-dev
-$APT_GET_INSTALL libaudio-dev
-$APT_GET_INSTALL libqt5opengl5-dev
-$APT_GET_INSTALL libqt5svg5-dev
-$APT_GET_INSTALL libqt5webkit5-dev
-$APT_GET_INSTALL libqt5websockets5-dev
-$APT_GET_INSTALL libqt5x11extras5-dev
-$APT_GET_INSTALL libqt5xmlpatterns5-dev
-$APT_GET_INSTALL libqt5waylandclient5-dev
-$APT_GET_INSTALL libqt5webenginewidgets5
-$APT_GET_INSTALL libqt5webview5-dev
-$APT_GET_INSTALL qtwebengine5-dev
-$APT_GET_INSTALL libphonon4qt5-dev
-$APT_GET_INSTALL qttools5-dev
-$APT_GET_INSTALL qtmultimedia5-dev
-$APT_GET_INSTALL libnifti-dev
-$APT_GET_INSTALL x11proto-print-dev
-$APT_GET_INSTALL libopenjp2-7-dev
-$APT_GET_INSTALL libqwt-qt5-dev
-$APT_GET_INSTALL qtpositioning5-dev
-$APT_GET_INSTALL libqt5sensors5-dev
-$APT_GET_INSTALL libqt5webchannel5-dev
-$APT_GET_INSTALL libdouble-conversion-dev
-$APT_GET_INSTALL libgraphite2-dev
-$APT_GET_INSTALL libharfbuzz-dev
-$APT_GET_INSTALL libhyphen-dev
-## TODO(ylep): dev packages after this mark have not been reviewed and their corresponding runtime package added to casa-run
-$APT_GET_INSTALL libwebp-dev
-$APT_GET_INSTALL libwoff-dev
-$APT_GET_INSTALL libxcb-keysyms1-dev
-$APT_GET_INSTALL libxcb-randr0
-$APT_GET_INSTALL libxcb-render-util0
-$APT_GET_INSTALL libxcb-render0-dev
-$APT_GET_INSTALL libxcb-shape0-dev
-$APT_GET_INSTALL libxcb-shm0-dev
-$APT_GET_INSTALL libxcb-sync-dev
-$APT_GET_INSTALL libxcb-util-dev
-$APT_GET_INSTALL libxcb-xfixes0-dev
-$APT_GET_INSTALL libxcb-xkb-dev
-$APT_GET_INSTALL libxcb-image0-dev
-$APT_GET_INSTALL libxcb-icccm4-dev
-$APT_GET_INSTALL libxcb-render-util0-dev
-$APT_GET_INSTALL libxcb-xinerama0-dev
-$APT_GET_INSTALL default-libmysqlclient-dev
-$APT_GET_INSTALL libarmadillo-dev
-$APT_GET_INSTALL libarpack2-dev
-$APT_GET_INSTALL libavcodec-dev
-$APT_GET_INSTALL libavutil-dev
-$APT_GET_INSTALL libboost-all-dev
-$APT_GET_INSTALL libdap-dev
-$APT_GET_INSTALL libeigen3-dev
-$APT_GET_INSTALL libepsilon-dev
-$APT_GET_INSTALL libflann-dev
-$APT_GET_INSTALL libfreexl-dev
-$APT_GET_INSTALL libfyba-dev
-$APT_GET_INSTALL libgdal-dev
-$APT_GET_INSTALL libgeos-dev
-$APT_GET_INSTALL libgeotiff-dev
-$APT_GET_INSTALL libgif-dev
-$APT_GET_INSTALL libgl2ps-dev
-$APT_GET_INSTALL libavformat-dev
-$APT_GET_INSTALL libhdf4-alt-dev
-$APT_GET_INSTALL libhdf5-mpi-dev
-$APT_GET_INSTALL libhdf5-openmpi-dev
-$APT_GET_INSTALL libhwloc-dev
-$APT_GET_INSTALL libibverbs-dev
-$APT_GET_INSTALL libjson-c-dev
-$APT_GET_INSTALL libjsoncpp-dev
-$APT_GET_INSTALL libkml-dev
-$APT_GET_INSTALL libminizip-dev
-$APT_GET_INSTALL libmysqlclient-dev
-$APT_GET_INSTALL libnetcdf-cxx-legacy-dev
-$APT_GET_INSTALL libnuma-dev
-$APT_GET_INSTALL libogdi3.2-dev
-$APT_GET_INSTALL libogg-dev
-$APT_GET_INSTALL libopenni-dev
-$APT_GET_INSTALL libopenni2-dev
-$APT_GET_INSTALL libpcl-dev
-$APT_GET_INSTALL libpoppler-dev
-$APT_GET_INSTALL libpoppler-private-dev
-$APT_GET_INSTALL libpq-dev
-$APT_GET_INSTALL libproj-dev
-$APT_GET_INSTALL libqhull-dev
-$APT_GET_INSTALL libspatialite-dev
-$APT_GET_INSTALL libsuperlu-dev
-$APT_GET_INSTALL libswresample-dev
-$APT_GET_INSTALL libswscale-dev
-$APT_GET_INSTALL libtheora-dev
-$APT_GET_INSTALL liburiparser-dev
-$APT_GET_INSTALL libvtk6-dev
-$APT_GET_INSTALL libvtk6-qt-dev
-$APT_GET_INSTALL libxerces-c-dev
-$APT_GET_INSTALL libxss-dev
-$APT_GET_INSTALL mpi-default-dev
-$APT_GET_INSTALL unixodbc-dev
-$APT_GET_INSTALL uuid-dev
-$APT_GET_INSTALL x11proto-scrnsaver-dev
+brainvisa_toolboxes_dev_dependencies=(
+)
 
-$APT_GET clean
+
+# Other dependencies of BrainVISA projects, which are not directly referred to
+# in the projects' CMakeLists.txt, but are packaged as third-party libraries.
+#
+# FIXME: this category should eventually disappear: if these are really
+# dependencies, they should be added to the corresponding CMakeList.txt
+brainvisa_probable_dev_dependencies=(
+    libblas-dev
+    libeigen3-dev
+    libhdf5-mpi-dev
+    liblapack-dev
+    libnifti-dev
+    libpcl-dev
+    libqt5webkit5-dev
+    mpi-default-dev
+)
+
+sudo apt-get -o Acquire::Retries=10 install --no-install-recommends -y \
+    ${brainvisa_standard_dev_dependencies[@]} \
+    ${brainvisa_toolboxes_dev_dependencies[@]} \
+    ${brainvisa_probable_dev_dependencies[@]}
+
+
+sudo apt-get clean
 # delete all the apt list files since they're big and get stale quickly
 sudo rm -rf /var/lib/apt/lists/*
