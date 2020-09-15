54a914ccbdc503653e56afee88a4f3d9ed6e4d2c
Yann Leprince <yann.leprince@cea.fr>
2020-09-15 16:35:03 +0200

    Install Singularity directly in /usr/local

--- share/scripts/build_singularity_3.sh
+++ share/scripts/build_singularity_3.sh
@@ -7,11 +7,9 @@ pushd /tmp
 
 # Assign default values
 : ${OS:=linux} ${ARCH:=amd64} ${SUDO:=sudo}
+: ${SINGULARITY_VERSION=3.6.2} ${GO_VERSION:=1.15.2}
+: ${INSTALL_PREFIX=/usr/local}
 
-
-if [ -z "${INSTALL_PREFIX}" ]; then
-    INSTALL_PREFIX=/usr/local
-fi
 
 if [ -z "${INSTALL_GO}" ]; then
     if [ -z "$(which go)" ]; then
@@ -21,17 +19,11 @@ fi
 
 if [ "${INSTALL_GO}" -eq "1" ]; then
     # Install go
-    echo "Installing go ..."
-    export GO_VERSION=1.13
-    if [ -z "${GO_INSTALL_PREFIX}" ]; then
-        GO_INSTALL_PREFIX=${INSTALL_PREFIX}/go-${GO_VERSION}
-    fi
+    echo "Installing go ${GO_VERSION} in /tmp/go ..."
     wget https://dl.google.com/go/go${GO_VERSION}.${OS}-${ARCH}.tar.gz
     ${SUDO} tar -C /tmp -xzf go${GO_VERSION}.${OS}-${ARCH}.tar.gz
-    ${SUDO} mv -f /tmp/go ${GO_INSTALL_PREFIX}
     rm go${GO_VERSION}.${OS}-${ARCH}.tar.gz
-    PATH="${GO_INSTALL_PREFIX}/bin:${PATH}"
-    [ -z "${GOPATH}" ] && GOPATH="${GO_INSTALL_PREFIX}"
+    PATH=/tmp/go/bin:${PATH}
 fi
 
 if [ -z "${GOPATH}" ]; then
@@ -42,20 +34,17 @@ fi
 export PATH GOPATH
 
 # Install singularity 3.6.2
-echo "Installing singularity ..."
-SINGULARITY_VERSION=3.6.2
-if [ -z "${SINGULARITY_INSTALL_PREFIX}" ]; then
-    SINGULARITY_INSTALL_PREFIX=${INSTALL_PREFIX}/singularity-${SINGULARITY_VERSION}
-fi
-
+echo "Installing singularity ${SINGULARITY_VERSION} ..."
 wget https://github.com/hpcng/singularity/releases/download/v${SINGULARITY_VERSION}/singularity-${SINGULARITY_VERSION}.tar.gz -O singularity-${SINGULARITY_VERSION}.tar.gz
 tar -zxf singularity-${SINGULARITY_VERSION}.tar.gz
 rm singularity-${SINGULARITY_VERSION}.tar.gz
 pushd singularity
-./mconfig --prefix=${SINGULARITY_INSTALL_PREFIX}
+./mconfig --prefix="${INSTALL_PREFIX}"
 make -C ./builddir
 ${SUDO} make -C ./builddir install
 popd
 rm -rf singularity
+rm -rf /tmp/go
+rm -rf /tmp/.go
 
 popd
