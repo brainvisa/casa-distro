acdd5b6548ecc6bb602dd87290d70921fef35cd3
Denis Rivière <denis.riviere.gm@gmail.com>
2020-09-21 18:04:34 +0200

    if svn.secret is not present, use svn the "normal" way, not forcing the non-interactive mode
    Fixes: #31

--- python/casa_distro/environment.py
+++ python/casa_distro/environment.py
@@ -557,29 +557,47 @@ def setup_dev(metadata,
            osp.join(output, i), verbose=verbose)
 
     svn_secret = osp.join(output, 'host', 'conf', 'svn.secret')
-    if not os.path.exists(svn_secret):
-        print('\n------------------------------------------------------------')
-        print('**WARNING:**')
-        print('Before using "casa_distro bv_maker" you will have to '
-              'create the svn.secret file with your Bioproj login / password '
-              'in order to access the BrainVisa repository.')
-        print('Place it at the following location:\n')
-        print(svn_secret)
-        print('\nThis file is a shell script that must set the variables '
-              'SVN_USERNAME and SVN_PASSWORD. Do not forget to properly quote '
-              'the values if they contains special characters.')
-        print('For instance, the file could contain the two following lines '
-              '(replacing "your_login" and "your_password" by appropriate '
-              'values:\n')
-        print("SVN_USERNAME='your_login'")
-        print("SVN_PASSWORD='your_password'\n")
-        print('If you are only using open-source projects, you can use the '
-              '"public" login/password: brainvisa / Soma2009\n')
-        print('Remember also that you can edit and customize the projects to '
-              'be built, by editing the following file:\n')
-        print(osp.join(output, 'host', 'conf', 'bv_maker.cfg'))
-        print('------------------------------------------------------------')
-        print()
+    # if not os.path.exists(svn_secret):
+    print('\n------------------------------------------------------------')
+    print('** WARNING: svn secret **')
+    print('Before using "casa_distro bv_maker" you will have to '
+          'setup svn to access the Biporoj server, which needs a login '
+          'and a password.\n'
+          'There are 2 methods for this, and 2 situations, which we could '
+          'simplify as this:\n\n'
+          '* opensource distro: if you are only using open-source '
+          'projects, you can use the preconfigured "public" '
+          'login/password: brainvisa / Soma2009.\n'
+          'Credentials are stored in the followinf file:\n')
+    print(svn_secret)
+    print('\nYou may leave it as is or replace with your own login/password '
+          'if you need to access restricted resources. Svn will be used '
+          'non-interactively, it will not ask for password confirmation '
+          '/ storage, but will reject any interactive input, including '
+          'commit comments etc.')
+    print('This file is a shell script that must set the variables '
+          'SVN_USERNAME and SVN_PASSWORD. Do not forget to properly quote '
+          'the values if they contains special characters.')
+    print('For instance, the file could contain the two following lines '
+          '(replacing "your_login" and "your_password" by appropriate '
+          'values:\n')
+    print("SVN_USERNAME='your_login'")
+    print("SVN_PASSWORD='your_password'\n")
+    print('If you need more interaction, then remove the svn.secret file, '
+          'and let svn interactively ask you for login/password and store '
+          'it appropriately, like in the following case.\n')
+    print('* brainvisa and other non-totally opensource distros: they '
+          'need a personal login and password. You can either use the '
+          'above svn.secret method (create the file if it doesn\'t exist '
+          'and fill in your information), or let svn interactively ask '
+          'you a login and password, and let it store it the way it suits '
+          'it. In this mode svn is used "directly", without interactive '
+          'restrictions.\n\n')
+    print('Remember also that you can edit and customize the projects to '
+          'be built, by editing the following file:\n')
+    print(osp.join(output, 'host', 'conf', 'bv_maker.cfg'))
+    print('------------------------------------------------------------')
+    print()
 
 
 def update_environment(config, base_directory, writable, verbose):
