8910a7b673576795711bb381dc9e618254526fe3
Denis Rivière <denis.riviere.gm@gmail.com>
2020-11-19 17:03:52 +0100

    improved the config GUI, allow to edit the config file

--- python/casa_distro/configuration_gui.py
+++ python/casa_distro/configuration_gui.py
@@ -1,12 +1,41 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+This GUI should run inside the container. It is typically started by the
+`bv`command on host side, which runs it inside the container.
+'''
+
+from __future__ import print_function
+
 import sys
 import json
 import subprocess
+import os
+import collections
 
-from soma.qt_gui.qt_backend import Qt
-from soma.qt_gui.qt_backend.QtCore import pyqtSignal
+try:
+    from soma.qt_gui.qt_backend import Qt
+    from soma.qt_gui.qt_backend.QtCore import Signal
+except ImportError:
+    try:
+        from PyQt5 import Qt
+        from PyQt5.QtCore import pyqtSignal as Signal
+    except ImportError:
+        from PyQt4 import Qt
+        from PyQt4.QtCore import pyqtSignal as Signal
+    if not Qt.QApplication.instance():
+        app = Qt.QApplication(sys.argv)
+    Qt.QMessageBox.warning(
+        None, 'Problem',
+        'soma-base is not installed.\n'
+        'Either you are running this program outside of a Casa-Distro '
+        'container, which is not the way it is designed to run, or soma-base '
+        'is not properly built / installed in the container. This may happen '
+        'in a developer container before its first build.\n\n'
+        'As a result, some configuration edition capabilities will not be '
+        'available.\n\n'
+        'You normally just run the "bv" script from the host.')
 
 
 class CasaLauncher(Qt.QDialog):
@@ -22,9 +51,26 @@ class CasaLauncher(Qt.QDialog):
         self.setup_links()
 
     def setup_ui(self):
+        self.setWindowTitle('CASA environment configuration')
         self._main_layout = Qt.QVBoxLayout(self)
 
+        env_path = os.environ.get('CASA_HOST_DIR', None)
+        if not env_path:
+            env_path, _, _ = get_env_path()
+            if not env_path:
+                env_path = '&lt;none&gt;'
+            else:
+                env_path = os.path.dirname(env_path)
+        self._main_layout.addWidget(Qt.QLabel(
+            '<b>environment host path:</b> %s' % env_path))
         self._mount_manager = MountManager(self.conf)
+
+        conf_line = Qt.QHBoxLayout()
+        conf_line.addWidget(Qt.QLabel('<b>Configuration:</b>'))
+        conf_line.addStretch(1)
+        self._conf_btn = Qt.QPushButton('...')
+        conf_line.addWidget(self._conf_btn)
+
         self._launchers = Launchers()
 
         self._errors_label = Qt.QLabel()
@@ -32,7 +78,9 @@ class CasaLauncher(Qt.QDialog):
         self._validation_btns = Qt.QDialogButtonBox(
             Qt.QDialogButtonBox.Ok | Qt.QDialogButtonBox.Cancel)
 
+        self._main_layout.addWidget(Qt.QLabel('<b>Mount points:</b>'))
         self._main_layout.addWidget(self._mount_manager)
+        self._main_layout.addLayout(conf_line)
         self._main_layout.addWidget(self._launchers)
         self._main_layout.addWidget(self._errors_label)
         self._main_layout.addWidget(self._validation_btns)
@@ -42,6 +90,7 @@ class CasaLauncher(Qt.QDialog):
         self._validation_btns.rejected.connect(self.close)
         self._mount_manager.valueChanged.connect(self.block_launchers)
         self._launchers.launched.connect(self.close_and_launch)
+        self._conf_btn.clicked.connect(self.edit_configuration)
 
     def save_conf(self):
         if self._mount_manager.check_all_mounts():
@@ -62,6 +111,27 @@ class CasaLauncher(Qt.QDialog):
     def block_launchers(self):
         self._launchers.disable_for_reload()
 
+    def edit_configuration(self):
+        dialog = Qt.QDialog(self)
+        layout = Qt.QVBoxLayout()
+        dialog.setLayout(layout)
+        config_edit = ConfigEditor(self.conf)
+        validation_btns = Qt.QDialogButtonBox(
+            Qt.QDialogButtonBox.Ok | Qt.QDialogButtonBox.Cancel)
+
+        layout.addWidget(config_edit)
+        layout.addWidget(validation_btns)
+
+        validation_btns.accepted.connect(dialog.accept)
+        validation_btns.rejected.connect(dialog.reject)
+
+        if dialog.exec_():
+            for k in list(self.conf.keys()):
+                del self.conf[k]
+            self.conf.update(config_edit.edited_config())
+            self._mount_manager.update_ui()
+            self.block_launchers()
+
     # def closeEvent(self, *args, **kwargs):
     #     super(Qt.QDialog, self).closeEvent(*args, **kwargs)
     #     print('CLOSE!!!!')
@@ -80,7 +150,7 @@ class MountManager(Qt.QWidget):
         self.setup_ui()
         self.setup_links()
 
-    valueChanged = pyqtSignal()
+    valueChanged = Signal()
 
     def setup_ui(self):
         self._main_layout = Qt.QVBoxLayout(self)
@@ -94,13 +164,7 @@ class MountManager(Qt.QWidget):
         self._mount_table.horizontalHeader().setSectionResizeMode(
             1, Qt.QHeaderView.Stretch)
 
-        for idx, (host, container) in enumerate(
-                self.conf.get('mounts', {}).items()):
-            self._mount_table.setItem(idx, 0, Qt.QTableWidgetItem(host))
-            # First col not editable
-            self._mount_table.item(idx, 0).setFlags(
-                self._mount_table.item(idx, 0).flags() ^ Qt.Qt.ItemIsEditable)
-            self._mount_table.setItem(idx, 1, Qt.QTableWidgetItem(container))
+        self.update_ui()
 
         self._error_label = Qt.QLabel()
 
@@ -119,6 +183,16 @@ class MountManager(Qt.QWidget):
         self._add_mount.clicked.connect(self._add_mount_row)
         self._remove_mount.clicked.connect(self._delete_mount_row)
         self._mount_table.cellChanged.connect(self._value_modified)
+
+    def update_ui(self):
+        self._mount_table.clearContents()
+        for idx, (host, container) in enumerate(
+                self.conf.get('mounts', {}).items()):
+            self._mount_table.setItem(idx, 0, Qt.QTableWidgetItem(host))
+            # First col not editable
+            self._mount_table.item(idx, 0).setFlags(
+                self._mount_table.item(idx, 0).flags() ^ Qt.Qt.ItemIsEditable)
+            self._mount_table.setItem(idx, 1, Qt.QTableWidgetItem(container))
 
     def _add_mount_row(self):
         host_mount_choice = Qt.QFileDialog()
@@ -160,7 +234,7 @@ class MountManager(Qt.QWidget):
             return
         host = self._mount_table.item(row, 0).text()
         cont = self._mount_table.item(row, 1).text()
-        self.conf['mounts'][host] = cont
+        self.conf.setdefault('mounts', {})[host] = cont
         self.valueChanged.emit()
         self.check_all_mounts()
 
@@ -214,7 +288,7 @@ class MountManager(Qt.QWidget):
 
 class Launchers(Qt.QWidget):
 
-    launched = pyqtSignal(str)
+    launched = Signal(str)
 
     def __init__(self):
         super(Launchers, self).__init__()
@@ -234,15 +308,17 @@ class Launchers(Qt.QWidget):
 
         self._reload_msg = Qt.QLabel()
 
+        env_path, python_path, build_path = get_env_path()
         brainvisa_icon = Qt.QPixmap(
-            '/casa/host/build/share/doc/axon/images/brainvisa.png')
+            os.path.join(build_path, 'share/doc/axon/images/brainvisa.png'))
         self._brainvisa_btn = Qt.QPushButton(
             Qt.QIcon(brainvisa_icon), 'BRAINVISA')
         self._brainvisa_btn.setIconSize(
             Qt.QSize(self.icon_size, self.icon_size))
 
         anatomist_icon = Qt.QPixmap(
-            '/casa/host/build/share/doc/anatomist-4.6/html/images/anaLogo.png')
+            os.path.join(build_path,
+                         'share/doc/anatomist/html/images/anaLogo.png'))
         self._anatomist_btn = Qt.QPushButton(
             Qt.QIcon(anatomist_icon), 'ANATOMIST')
         self._anatomist_btn.setIconSize(
@@ -281,10 +357,87 @@ class Launchers(Qt.QWidget):
             "Reloading is needed to launch softwares/terminal!")
 
 
-if __name__ == "__main__":
-    app = Qt.QApplication(sys.argv)
+class ConfigEditor(Qt.QWidget):
 
-    conf_path = '/casa/host/conf/casa_distro.json'
+    def __init__(self, conf, parent=None):
+        super(Qt.QWidget, self).__init__(parent)
+        try:
+            from soma.controller import OpenKeyController
+            import traits.api as traits
+        except ImportError as e:
+            print(e, file=sys.stderr)
+            return
+
+        self.conf = conf
+        self.controller = OpenKeyController()
+
+        trait_types = {
+            dict: traits.Dict,
+            collections.OrderedDict: traits.Dict,
+            str: traits.Str,
+            float: traits.Float,
+            int: traits.Int,
+            list: traits.List,
+            traits.TraitListObject: traits.List,
+        }
+        for key, value in conf.items():
+            trait_type = trait_types.get(type(value), traits.Any)
+            self.controller.add_trait(key, trait_type())
+            setattr(self.controller, key, value)
+
+        self.setup_ui()
+
+    def setup_ui(self):
+        from soma.qt_gui.controller_widget import ScrollControllerWidget
+        self.controller_widget = ScrollControllerWidget(self.controller,
+                                                        live=True)
+        layout = Qt.QVBoxLayout()
+        self.setLayout(layout)
+        layout.addWidget(self.controller_widget)
+
+    def edited_config(self):
+        new_conf = self.controller.export_to_dict()
+        if 'protected_parameters' in new_conf:
+            del new_conf['protected_parameters']
+        return new_conf
+
+
+def get_env_path():
+    build_path = None
+    casa_path = os.path.dirname(os.path.dirname(os.path.dirname(
+        os.path.realpath(__file__))))
+    if os.path.isdir(os.path.join(casa_path, 'python')):
+        python_path = os.path.join(casa_path, 'python')
+    else:
+        try:
+            import casa_distro
+            python_path = os.path.dirname(os.path.dirname(
+                casa_distro.__file__))
+        except ImportError:
+            python_path = None
+    # try direct install case (unlinkely)
+    env_path = casa_path
+    if os.path.exists(os.path.join(env_path, 'conf', 'casa_distro.json')):
+        build_path = os.path.dirname(python_path)
+    else:
+        # install [/ build] case
+        env_path = os.path.dirname(env_path)
+        if os.path.exists(os.path.join(env_path, 'conf', 'casa_distro.json')):
+            build_path = os.path.dirname(python_path)
+        else:
+            # source case
+            env_path = os.path.dirname(os.path.dirname(
+                os.path.dirname(env_path)))
+            build_path = os.path.join(env_path, 'build')
+    return env_path, python_path, build_path
+
+
+if __name__ == "__main__":
+    if not Qt.QApplication.instance():
+        app = Qt.QApplication(sys.argv)
+
+    casa_path, _, _ = get_env_path()
+    conf_path = os.path.join(casa_path, 'conf/casa_distro.json')
     dialog = CasaLauncher(conf_path)
     dialog.show()
 
