brainvisa/casa-distro

commit 5a3bfbb0f4732cf9e56b8267c62c2ae21172b812

Skip non-executable files for bv symlinks

python/casa_distro/container_environment.py
@@ -7,6 +7,7 @@ import os
 import os.path as osp
 import platform
 import shutil
+import stat
 import sys
 import time
 import subprocess
@@ -62,10 +63,13 @@ def create_environment_bin_commands(source, dest):
     for command in commands:
         if command in exclude_from_bin:
             continue
-        script = osp.join(dest, command)
-        if osp.exists(script):
-            os.remove(script)
-        os.symlink('bv', script)
+        source_command = osp.join(source, command)
+        if not os.stat(source_command).st_mode & stat.S_IXUSR:
+            continue  # skip non-executable files (e.g. bv_env.sh)
+        dest_link = osp.join(dest, command)
+        if osp.exists(dest_link):
+            os.remove(dest_link)
+        os.symlink('bv', dest_link)
 
 
 def download_install(install_dir, distro, version, url):

commit fb29f8db5a9887db6c364134eac7a90e7ab81f58

Fix creation of bv symlinks
My bad, I broke it in 5a3bfbb0f4732cf9e56b8267c62c2ae21172b812

python/casa_distro/container_environment.py
@@ -64,8 +64,13 @@ def create_environment_bin_commands(source, dest):
         if command in exclude_from_bin:
             continue
         source_command = osp.join(source, command)
-        if not os.stat(source_command).st_mode & stat.S_IXUSR:
-            continue  # skip non-executable files (e.g. bv_env.sh)
+        try:
+            if not os.stat(source_command).st_mode & stat.S_IXUSR:
+                continue  # skip non-executable files (e.g. bv_env.sh)
+        except OSError:
+            # avoid skipping commands that do not have a binary (casa_distro
+            # and casa_distro_admin)
+            pass
         dest_link = osp.join(dest, command)
         if osp.exists(dest_link):
             os.remove(dest_link)