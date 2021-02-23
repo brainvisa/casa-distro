685027eb67b757220841bdd51ae446af74abae74
Denis Rivière <denis.riviere.gm@gmail.com>
2021-02-23 11:55:45 +0100

    fixed another bug in image selection

--- python/casa_distro/admin_commands.py
+++ python/casa_distro/admin_commands.py
@@ -1126,7 +1126,7 @@ def bbi_daily(type=None, distro=None, branch=None, system=None,
             for image in images:
                 run_image = get_run_image(image, base_directory=base_directory)
                 if run_image:
-                    run_images.append(run_image)
+                    run_images.append(run_image[1])
             if bbi_daily.update_base_images(list(images) + run_images):
                 succesful_tasks.append('update_base_images')
             else:
