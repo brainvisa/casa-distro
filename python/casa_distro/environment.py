d8d1b2188ceb06a4e59201601cd1b63cb05a058f
Yann Leprince <5501307+ylep@users.noreply.github.com>
2021-08-03 09:32:24 +0200

    Remove /shfj from the list of standard directories
    /shfj is not available on CEA networks anymore, see https://github.com/brainvisa/brainvisa.github.io/discussions/62#discussioncomment-1087399
    
    (cherry picked from commit 965982b25c1ca789f868c7ef054bc6b59dafe7e0)

--- python/casa_distro/environment.py
+++ python/casa_distro/environment.py
@@ -587,7 +587,7 @@ def standard_dirs_to_mount():
       software.
     """
     standard_dirs = (['/home', '/mnt', '/media', '/srv',
-                      '/neurospin', '/i2bm', '/shfj', '/nfs']
+                      '/neurospin', '/i2bm', '/nfs']
                      + glob('/volatile*'))
     for dirname in standard_dirs:
         if os.path.isdir(dirname) and not os.path.islink(dirname):
