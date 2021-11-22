12c0323d00de7e861de37e2329cd6c39abecc7a5
Denis Rivière <denis.riviere.gm@gmail.com>
2021-11-22 18:57:36 +0100

    update qtconsole module (#287)

--- image-recipes/casa-run/5.1/install_pip_dependencies.sh
+++ image-recipes/casa-run/5.1/install_pip_dependencies.sh
@@ -41,3 +41,4 @@ ${PIP_INSTALL} torch-vision
 ${PIP_INSTALL} http://bonsai.hgc.jp/~mdehoon/software/cluster/Pycluster-1.59.tar.gz
 
 ${PIP_INSTALL} ipywidgets ipycanvas ipyevents jupyter jupyterlab_widgets jupyter_console # widgetsnbextensions
+${PIP_INSTALL} -U qtconsole
