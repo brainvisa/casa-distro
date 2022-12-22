c94b562f606d73be3911377f599129c40464ed32
Yann Leprince <yann.leprince@cea.fr>
2022-12-22 19:05:57 +0100

    Bump version to 3.2.0

--- python/casa_distro/info.py
+++ python/casa_distro/info.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import, division, print_function
 
 # casa-distro version
 version_major = 3
-version_minor = 1
+version_minor = 2
 version_micro = 0
 version_extra = ""
 
