2ebd4ddb2e577c592b81b3b9b6533c4c4a984cce
sapetnioc <sapetnioc@users.noreply.github.com>
2023-01-26 09:31:16 +0100

    #300 rewrite pull_image

--- python/casa_distro/defaults.py
+++ python/casa_distro/defaults.py
@@ -12,8 +12,5 @@ publish_server = os.environ.get('BRAINVISA_PUBLISH_SERVER', 'brainvisa.info')
 publish_login = os.environ.get('BRAINVISA_PUBLISH_LOGIN', 'brainvisa')
 publish_dir = os.environ.get('BRAINVISA_PUBLISH_DIR',
                              '/var/www/html/brainvisa.info_download')
-publish_url = '{publish_login}@{publish_server}:{publish_dir}/'.format(
-      publish_login=publish_login,
-      publish_server=publish_server,
-      publish_dir=publish_dir)
-default_download_url = 'https://{}/download'.format(publish_server)
+publish_url = f'{publish_login}@{publish_server}:{publish_dir}/'
+default_download_url = f'https://{publish_server}/download'
